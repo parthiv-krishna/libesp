dd32152def5393291c79b75715c221293179cd50
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-05-13 03:59:56 -0700

    working gpio.c and .h

--- libesp/include/gpio.h
+++ libesp/include/gpio.h
@@ -3,15 +3,11 @@
 
 #include "libesp.h"
 
-typedef enum {
-    GPIO_MODE_INPUT, 
-    GPIO_MODE_OUTPUT
-} gpio_mode_t;
+void gpio_set_output(int pin);
+void gpio_write(int pin, bool value);
+void gpio_toggle(int pin);
 
 void gpio_set_input(int pin);
-void gpio_set_output(int pin);
-void gpio_set_mode(int pin, gpio_mode_t mode);
-
-void gpio_write(int pin, bool value);
+bool gpio_read(int pin);
 
 #endif // GPIO_H
--- libesp/src/gpio.c
+++ libesp/src/gpio.c
@@ -1,40 +1,45 @@
 #include "gpio.h"
 
-enum { 
-    GPIO_OUT_REG = ESP32C3_GPIO + 0x4,
-    GPIO_OUT_EN = ESP32C3_GPIO + 0x20, 
-    GPIO_OUT_FUNC_BASE = ESP32C3_GPIO + 0x554,
-    GPIO_IN_FUNC = 85 
+enum {
+    GPIO_IN = ESP32C3_GPIO + 0x3C,
+    GPIO_OUT = ESP32C3_GPIO + 0x4,
+    GPIO_OUT_EN = ESP32C3_GPIO + 0x20,
+    GPIO_OUT_FUNC_BASE = ESP32C3_GPIO + 0x554
 };
 
-void gpio_set_input(int pin) {
-    // disable output
-    uint32_t gpio_out_en = get32(GPIO_OUT_EN);
-    gpio_out_en &= ~(1 << pin);
-    put32(GPIO_OUT_EN, gpio_out_en);
-}
-
 void gpio_set_output(int pin) {
-    // TRM 5.5.3: configure pin function to respond to GPIO_OUT_EN
-    put32(GPIO_OUT_FUNC_BASE + pin*sizeof(uint32_t), (1 << 9) | 0x80);
-}
+    // TRM section 5.5.3
+    uint32_t func_reg_addr = GPIO_OUT_FUNC_BASE + pin * sizeof(uint32_t);
+    put32(func_reg_addr, (1 << pin) | 128);
 
-void gpio_set_mode(int pin, gpio_mode_t mode) {
-    if (mode == GPIO_MODE_INPUT) {
-        gpio_set_input(pin);
-    } else if (mode == GPIO_MODE_OUTPUT) {
-        gpio_set_output(pin);
-    }
+    // enable output
+    uint32_t out_en = get32(GPIO_OUT_EN);
+    out_en |= (1 << pin); // enable output on pin
+    put32(GPIO_OUT_EN, out_en);
 }
 
 void gpio_write(int pin, bool value) {
-    // enable output
-    uint32_t gpio_out_reg = get32(GPIO_OUT_REG);
-    gpio_out_reg &= ~(1 << pin); // clear
-    gpio_out_reg |= (value ? 1 : 0) << pin;
-    put32(GPIO_OUT_REG, gpio_out_reg);
+    uint32_t out = get32(GPIO_OUT);
+    out &= ~(1 << pin);    // clear existing
+    out |= (value << pin); // set new
+    put32(GPIO_OUT, out);
 }
 
-// void gpio_toggle(int pin) { REG(C3_GPIO)[1] ^= BIT(pin); }
+void gpio_toggle(int pin) {
+    uint32_t out = get32(GPIO_OUT);
+    out ^= (1 << pin); // toggle
+    put32(GPIO_OUT, out);
+}
 
-// bool gpio_read(int pin) { return REG(C3_GPIO)[15] & BIT(pin) ? 1 : 0; }
+void gpio_set_input(int pin) {
+    // disable output
+    uint32_t out_en = get32(GPIO_OUT_EN);
+    out_en &= ~(1 << pin); // clear bit
+    put32(GPIO_OUT_EN, out_en);
+}
+
+bool gpio_read(int pin) {
+    // TRM section 5.4.4
+    uint32_t in = get32(GPIO_IN);
+    return (in >> pin) & 1;
+}
