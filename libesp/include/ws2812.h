25d70b0d97abacb0cc55c8b680fa9b02fddae400
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-06-01 16:09:26 -0700

    ws2812 strip

--- libesp/include/ws2812.h
+++ libesp/include/ws2812.h
@@ -9,7 +9,18 @@ typedef struct {
     uint8_t b;
 } rgb_t;
 
+#define RGB_MK(r, g, b) ((rgb_t) {(uint8_t)(r), (uint8_t)(g), (uint8_t)(b)})
 
-void ws2812_display_one(int pin, const rgb_t pixel);
+typedef struct {
+    unsigned pin;
+    unsigned num_leds;
+    rgb_t pixels[256];
+} ws2812_strip_t;
+
+void ws2812_display_one(unsigned pin, rgb_t pixel);
+
+ws2812_strip_t ws2812_strip_init(unsigned pin, int num_leds);
+void ws2812_strip_update(ws2812_strip_t *strip);
+void ws2812_strip_set_all(ws2812_strip_t *strip, rgb_t pixel);
 
 #endif // WS2812_H
--- libesp/src/ws2812.c
+++ libesp/src/ws2812.c
@@ -3,7 +3,7 @@
 #include "gpio.h"
 #include "timer.h"
 
-static inline void write_byte(int pin, uint8_t byte) {
+static inline void write_byte(unsigned pin, uint8_t byte) {
      // gives right timing, found with logic analyzer
     unsigned delays[2] = {22, 6};
     for (uint8_t mask = 0x80; mask; mask >>= 1) {
@@ -15,8 +15,40 @@ static inline void write_byte(int pin, uint8_t byte) {
     }
 }
 
-void ws2812_show(int pin, const rgb_t pixels) {
-    write_byte(pin, pixels.g);
-    write_byte(pin, pixels.r);
-    write_byte(pin, pixels.b);
+static inline void write_pixel(unsigned pin, rgb_t pixel) {
+    write_byte(pin, pixel.g);
+    write_byte(pin, pixel.r);
+    write_byte(pin, pixel.b);
+}
+
+static inline void set_signal(unsigned pin) {
+    gpio_write(pin, false);
+    timer_delay_us(500);
+}
+
+void ws2812_display_one(unsigned pin, rgb_t pixel) {
+    write_pixel(pin, pixel);
+    set_signal(pin);
+}
+
+ws2812_strip_t ws2812_strip_init(unsigned pin, int num_leds) {
+    gpio_set_output(pin);
+
+    return (ws2812_strip_t) {
+        .pin = pin,
+        .num_leds = num_leds,
+    };   
+}
+
+void ws2812_strip_update(ws2812_strip_t *strip) {
+    for (int i = 0; i < strip->num_leds; i++) {
+        write_pixel(strip->pin, strip->pixels[i]);
+    }
+    set_signal(strip->pin);
+}
+
+void ws2812_strip_set_all(ws2812_strip_t *strip, rgb_t pixel) {
+    for (int i = 0; i < strip->num_leds; i++) {
+        strip->pixels[i] = pixel;
+    }
 }
