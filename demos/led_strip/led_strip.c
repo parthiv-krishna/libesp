f24ce0c7ec5c39c99febaa02af2b40df16188ef6
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-06-02 02:14:21 -0700

    led strip demo

---
+++ demos/led_strip/led_strip.c
@@ -0,0 +1,28 @@
+#include "libesp.h"
+#include "ws2812.h"
+#include "rgb.h"
+#include "timer.h"
+#include "watchdog.h"
+
+#define PIN 8
+#define NUM_LEDS 256
+
+int main(void) {
+    watchdog_disable();
+
+    ws2812_strip_t ws2812_strip = ws2812_strip_init(PIN, NUM_LEDS);
+
+    rgb_t colors[360];
+    for (int h = 0; h < 360; h++) {
+        colors[h] = rgb_from_hsv(h, 100, 100);
+    }
+
+    while (1) {
+        for (int h = 0; h < 360; h++) {
+            for (int i = 0; i < NUM_LEDS; i++) {
+                ws2812_strip.pixels[i] = colors[(h + i) % 360];
+            }
+            ws2812_strip_update(&ws2812_strip);
+        }
+    }
+}
